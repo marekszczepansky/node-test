39988b3fa6b71beb7cc1201ce57e49b9d030a80b
Marek Szczepański <mszczepanski2@atlassian.com>
2021-10-27 23:05:02 +0200

    simplified error handling

--- components.js
+++ components.js
@@ -3,14 +3,14 @@ import fetch from 'node-fetch';
 const orphanComponents = await fetch("https://herocoders.atlassian.net/rest/api/3/project/IC/components")
     .then(response => response.json())
     .then(json => json.filter ? json.filter(({lead}) => !lead).map(({name}) => name) : [])
-    .catch(() => console.error("Component query error"))
     .then(components => Promise.all(components.map(name =>
         fetch("https://herocoders.atlassian.net/rest/api/3/search?fields=key&jql=" +
             encodeURIComponent(`project = IC and component="${name}"`))
             .then(response => response.json())
-            .then(({total}) => ({name, issues: total}))
-            .catch(() => console.error(`Issues search for component ${name} error`)))))
-    .catch(() => console.error("Obtaining issues for components error"));
+            .then(({total}) => ({name, issues: total})))))
+    .catch(() => console.error("Component query error"));
 
-console.log("Components without lead:");
-console.table(orphanComponents);
+if (orphanComponents) {
+    console.log("Components without lead:");
+    console.table(orphanComponents);
+}
