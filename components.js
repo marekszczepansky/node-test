f34ccf91aafcb81d72ad13844815c059feb6b09e
Marek Szczepański <mszczepanski2@atlassian.com>
2021-10-27 22:50:01 +0200

    readability fix

--- components.js
+++ components.js
@@ -2,18 +2,14 @@ import fetch from 'node-fetch';
 
 const orphanComponents = await fetch("https://herocoders.atlassian.net/rest/api/3/project/IC/components")
     .then(response => response.json())
-    .then(json => {
-        return json.filter ? json.filter(({lead}) => !lead).map(({name}) => name) : [];
-
-    })
+    .then(json => json.filter ? json.filter(({lead}) => !lead).map(({name}) => name) : [])
     .catch(() => console.error("Component query error"))
-    .then(components => Promise.all(components.map(name => {
-        return fetch("https://herocoders.atlassian.net/rest/api/3/search?fields=key&jql=" +
+    .then(components => Promise.all(components.map(name =>
+        fetch("https://herocoders.atlassian.net/rest/api/3/search?fields=key&jql=" +
             encodeURIComponent(`project = IC and component="${name}"`))
             .then(response => response.json())
-            .then(({total}) => ({name, issues:total}))
-            .catch(() => console.error(`Issues search for component ${name} error`));
-    })))
+            .then(({total}) => ({name, issues: total}))
+            .catch(() => console.error(`Issues search for component ${name} error`)))))
     .catch(() => console.error("Obtaining issues for components error"));
 
 console.log("Components without lead:");
